227a1b264b0448ae904cb86d2286cb003931e58b
Britt Cyr <cyrbritt@gmail.com>
2025-08-29 03:27:47 +0000

    u128 math

--- commons/src/math/u128x128_math.rs
+++ commons/src/math/u128x128_math.rs
@@ -2,7 +2,7 @@ use dlmm_interface::Rounding;
 use ruint::aliases::U256;
 
 /// (x * y) / denominator
-pub fn mul_div(x: u128, y: u128, denominator: u128, rounding: Rounding) -> Option<u128> {
+pub(crate) fn mul_div(x: u128, y: u128, denominator: u128, rounding: Rounding) -> Option<u128> {
     if denominator == 0 {
         return None;
     }
@@ -25,13 +25,36 @@ pub fn mul_div(x: u128, y: u128, denominator: u128, rounding: Rounding) -> Optio
 /// (x * y) >> offset
 #[inline]
 pub fn mul_shr(x: u128, y: u128, offset: u8, rounding: Rounding) -> Option<u128> {
-    let denominator = 1u128.checked_shl(offset.into())?;
-    mul_div(x, y, denominator, rounding)
+    /*
+        (a * b) >> 64
+            => (a * b) / (1<<64)
+            => if roundup ((a * b) + u64::MAX) >> 64
+               else (a * b) >> 64
+     */
+    match rounding {
+        Rounding::Up => {
+            ((x * y) + u64::MAX as u128).checked_shr(offset.into())
+        }
+        Rounding::Down => {
+            (x * y).checked_shr(offset.into())
+        }
+    }
 }
 
 /// (x << offset) / y
 #[inline]
 pub fn shl_div(x: u128, y: u128, offset: u8, rounding: Rounding) -> Option<u128> {
-    let scale = 1u128.checked_shl(offset.into())?;
-    mul_div(x, scale, y, rounding)
+    // Remove the multiple and just do a shift.
+    let x = U256::from(x);
+    let x_shifted = x.checked_shl(offset.into())?;
+
+    let y = U256::from(y);
+
+    match rounding {
+        Rounding::Up => x_shifted.div_ceil(y).try_into().ok(),
+        Rounding::Down => {
+            let (quotient, _) = x_shifted.div_rem(y);
+            quotient.try_into().ok()
+        }
+    }
 }
